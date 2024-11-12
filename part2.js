0c4919a53b5a7dc3ec2caf9196792064832359f8
SU-T <guitaristpeet@gmail.com>
2024-11-11 22:08:29 -0500

    created function to count number of columns

--- part2.js
+++ part2.js
@@ -8,51 +8,58 @@
 let str =
   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
-const output = [];
-const row = [];
-let cell = "";
-let commaCount = 0;
-
-for (let i = 0; i < str.length; i++) {
-  if (str[i] === ",") {
-    // if "," is found
-    // 1. Add the cell to the row
-    // 2. Increment commaCount for the next add
-    // 3. Reset cell
-    row[commaCount] = cell;
-    commaCount++;
-    cell = "";
-    continue;
-  } else if (str[i] === "\n") {
-    // if "\n" is found
-    // 1. Add the 4th cell to the row
-    // 2. Create a copy of row and push into the output array
-    // 3. Reset commaCount and cell
-    row[commaCount] = cell;
-    output.push([...row]);
-    commaCount = 0;
-    cell = "";
-    continue;
-  } else if (i + 1 === str.length) {
-    // When iterate over to the last letter of string
-    // 1. Add last letter to the cell
-    // 2. Add the 4th cell to the row
-    // 3. Create a copy of row and push into the output array
-    commaCount++;
-    cell += str[i];
-    row[commaCount] = cell;
-    output.push([...row]);
+// Function to count columns of string
+const countCol = (string) => {
+  let columns = 1;
+  for (let letter of string) {
+    if (letter === ",") {
+      columns++;
+    } else if (letter === "\n") {
+      break;
+    }
   }
-  cell += str[i];
-}
+  return columns;
+};
 
-console.log(output);
+// Variable that stores number of columns
+const colCount = countCol(str);
 
-// const printOutput = () => {
-//   output.forEach((obj) => {
-//     const eachRow = Object.values(obj);
-//     console.log(eachRow.join(" "));
-//   });
-// };
+// const output = [];
+// const row = [];
+// let cell = "";
+// let commaCount = 0;
 
-// printOutput();
+// for (let i = 0; i < str.length; i++) {
+//   if (str[i] === ",") {
+//     // if "," is found
+//     // 1. Add the cell to the row
+//     // 2. Increment commaCount for the next add
+//     // 3. Reset cell
+//     row[commaCount] = cell;
+//     commaCount++;
+//     cell = "";
+//     continue;
+//   } else if (str[i] === "\n") {
+//     // if "\n" is found
+//     // 1. Add the 4th cell to the row
+//     // 2. Create a copy of row and push into the output array
+//     // 3. Reset commaCount and cell
+//     row[commaCount] = cell;
+//     output.push([...row]);
+//     commaCount = 0;
+//     cell = "";
+//     continue;
+//   } else if (i + 1 === str.length) {
+//     // When iterate over to the last letter of string
+//     // 1. Add last letter to the cell
+//     // 2. Add the 4th cell to the row
+//     // 3. Create a copy of row and push into the output array
+//     commaCount++;
+//     cell += str[i];
+//     row[commaCount] = cell;
+//     output.push([...row]);
+//   }
+//   cell += str[i];
+// }
+
+// console.log(output);
