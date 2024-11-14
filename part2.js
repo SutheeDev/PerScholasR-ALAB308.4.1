a6856caa9796d5bc8fade5027022754b257b3eee
SU-T <guitaristpeet@gmail.com>
2024-11-13 22:17:25 -0500

    Implementation of part2

--- part2.js
+++ part2.js
@@ -8,10 +8,12 @@
 let str =
   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
+const output = [];
+
 // Function to count columns of string
 const countCol = (string) => {
   let columns = 1;
-  for (let letter of string) {
+  for (const letter of string) {
     if (letter === ",") {
       columns++;
     } else if (letter === "\n") {
@@ -21,45 +23,41 @@ const countCol = (string) => {
   return columns;
 };
 
-// Variable that stores number of columns
-const colCount = countCol(str);
+// Function to add row to the output
+const addRowToOutput = (string, columns) => {
+  const result = [];
+  let word = "";
+  let count = 0;
 
-// const output = [];
-// const row = [];
-// let cell = "";
-// let commaCount = 0;
+  const rowCharArr = string.split("");
 
-// for (let i = 0; i < str.length; i++) {
-//   if (str[i] === ",") {
-//     // if "," is found
-//     // 1. Add the cell to the row
-//     // 2. Increment commaCount for the next add
-//     // 3. Reset cell
-//     row[commaCount] = cell;
-//     commaCount++;
-//     cell = "";
-//     continue;
-//   } else if (str[i] === "\n") {
-//     // if "\n" is found
-//     // 1. Add the 4th cell to the row
-//     // 2. Create a copy of row and push into the output array
-//     // 3. Reset commaCount and cell
-//     row[commaCount] = cell;
-//     output.push([...row]);
-//     commaCount = 0;
-//     cell = "";
-//     continue;
-//   } else if (i + 1 === str.length) {
-//     // When iterate over to the last letter of string
-//     // 1. Add last letter to the cell
-//     // 2. Add the 4th cell to the row
-//     // 3. Create a copy of row and push into the output array
-//     commaCount++;
-//     cell += str[i];
-//     row[commaCount] = cell;
-//     output.push([...row]);
-//   }
-//   cell += str[i];
-// }
+  while (count < columns) {
+    let char = rowCharArr.shift();
+    if (char === ",") {
+      result.push(word);
+      word = "";
+      count++;
+    } else if (rowCharArr.length === 0) {
+      word += char;
+      result.push(word);
+      count++;
+    } else {
+      word += char;
+    }
+  }
+  output.push(result);
+};
 
-// console.log(output);
+const convertCSV = (csvStr) => {
+  // Variable that stores number of columns
+  const colCount = countCol(csvStr);
+
+  const strArr = csvStr.split("\n");
+
+  for (let i = 0; i < strArr.length; i++) {
+    addRowToOutput(strArr[i], colCount);
+  }
+  return output;
+};
+
+console.log(convertCSV(str));
